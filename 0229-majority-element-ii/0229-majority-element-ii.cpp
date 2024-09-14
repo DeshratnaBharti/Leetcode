037916d795092c1b7e7e1e24a7943d60f75538c8
DeshratnaBharti <133379373+DeshratnaBharti@users.noreply.github.com>
2024-09-14 09:30:49 +0630

    Time: 11 ms (41.54%), Space: 19.8 MB (20.39%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,10 +4,13 @@ public:
         int n = arr.size();
         vector<int>ans;
         unordered_map<int,int>m;
+          int mini = int(n / 3) + 1;
         for(int i=0;i<n;i++){
             m[arr[i]]++;
         
-            if(m[arr[i]] > floor(n/3)) ans.push_back(arr[i]);
+             if (m[arr[i]] == mini) {
+            ans.push_back(arr[i]);}
+             if (ans.size() == 2) break;
         }
         return ans;
 
