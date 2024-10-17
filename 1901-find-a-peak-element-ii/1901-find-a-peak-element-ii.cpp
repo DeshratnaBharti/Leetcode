66c0bcd549fdcaf06af566ed26e130e15d7be20a
DeshratnaBharti <133379373+DeshratnaBharti@users.noreply.github.com>
2024-10-17 09:00:18 +0630

    Time: 119 ms (76.93%), Space: 49.3 MB (77.76%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -17,12 +17,12 @@ public:
        int l=0,h=m-1;
        while(l<=h){
            int mid = l+(h-l)/2;
-           int maxRowindex = findmaxIndex(mat,m,n,mid);
+           int maxRowindex = findmaxIndex(mat,n,m,mid);
            int left = mid-1 >=0 ? mat[maxRowindex][mid-1] : -1;
            int right = mid+1< m ?  mat[maxRowindex][mid+1] : -1;
            if(mat[ maxRowindex][mid]>left and mat[ maxRowindex][mid]>right){
             return{maxRowindex,mid};
-           }else if(mat[ maxRowindex][mid]<left) h = mid;
+           }else if(mat[ maxRowindex][mid]<left) h = mid-1;
            else l=mid+1;
        }
        return {-1,-1};
