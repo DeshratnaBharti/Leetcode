cf83f3fdb24613e1a5d27fd3a90c6c16ad392de8
DeshratnaBharti <133379373+DeshratnaBharti@users.noreply.github.com>
2024-12-31 10:39:04 +0630

    Time: 4 ms (65.84%), Space: 10.9 MB (47.16%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string arr, int k) {
+      int  mp[26]={0};
+         int maxF =0;
+        int maxlen = 0;int l =0 ,r=0;
+       
+        while(r<arr.size()){
+            mp[arr[r]-'A']++;
+            maxF = max(maxF, mp[arr[r]-'A']);
+            
+                while((r-l+1) -maxF>k){
+                    mp[arr[l]-'A']--;
+                    maxF =0;
+                    for(int i=0;i<25;i++) maxF = max(maxF,mp[i]);
+                    l++;
+                }
+            
+            if((r-l+1) -maxF<=k) maxlen = max(maxlen,r-l+1);
+            r++;
+        }
+        return maxlen;
+    }
+};
