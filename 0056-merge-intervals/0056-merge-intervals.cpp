55370779262934f154d7408629130a53616564d1
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-02-06 09:29:45 +0630

    Time: 7 ms (44.22%), Space: 24.9 MB (11.61%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,20 @@
-bool cmp(vector<int>&a,vector<int>&b){
-    return a[1]<b[1];
-}
-    class Solution {
+class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        //sort karege ending time ke anussar
-         sort(intervals.begin(),intervals.end(),cmp);
-         vector<vector<int>>result;
-         int n= intervals.size();
-         result.push_back(intervals[n-1]);
-         for(int i=n-2;i>=0;i--){
-            vector<int>curr= intervals[i];//curr={1,3}
-            if(curr[1] >= result[result.size()-1][0]){
-                //curr.end >= last.start -> overlapping wala case hai
-                result[result.size()-1][0] = min (result[result.size()-1][0],curr[0]);
-                result[result.size()-1][1] = max (result[result.size()-1][1],curr[1]);
-
-            }else{
-               // overlapping nahin hua hai
-                result.push_back(intervals[i]);
-
+        sort(intervals.begin(),intervals.end());
+        int n = intervals.size();
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+        for(int i=1;i<n;i++){
+            vector<int>curr = intervals[i];
+            if(curr[0]<= ans[ans.size()-1][1]){
+                ans[ans.size()-1][0] = min(curr[0],ans[ans.size()-1][0]);
+                ans[ans.size()-1][1] = max(curr[1],ans[ans.size()-1][1]);
             }
-         }
-         reverse(result.begin(),result.end());
-         return result;
+            else{
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
     }
 };
