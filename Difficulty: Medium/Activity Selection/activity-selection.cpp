02e63347920c25cf2c8f5c9427a62297e1403f62
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-02-12 13:50:09 +0630

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Activity Selection/activity-selection.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int activitySelection(vector<int> &start, vector<int> &end) {
+      vector<pair<int,int>>temp;
+      int n = end.size();
+     
+      for(int i=0;i<n;i++){
+         temp.push_back({end[i],start[i]});
+         
+      }
+      sort(temp.begin(),temp.end());
+      int ans=1,last = temp[0].first;
+      for(int i=1;i<end.size();i++){
+          if(last<temp[i].second){
+              ans++;
+              last = temp[i].first;
+          }
+      }
+      return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<int> start;
+        int num;
+        while (ss >> num)
+            start.push_back(num);
+        getline(cin, input);
+        ss.clear();
+        vector<int> end;
+        ss.str(input);
+        while (ss >> num)
+            end.push_back(num);
+
+        Solution obj;
+        cout << obj.activitySelection(start, end) << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
