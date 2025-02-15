13e3a204316370c339f4cb16f8f532f4b86671d0
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-02-15 22:49:31 +0630

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -2,32 +2,27 @@
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-          vector<int> result; // Holds the postorder traversal result
-        // Loop until there are no nodes to process
+          vector<int> result; /
         while (root) {
-            // If the right subtree does not exist, process the current node and move to the left subtree
             if (!root->right) {
                 result.push_back(root->val);
                 root = root->left;
             } else {
-                // Find the rightmost node of the left subtree or the left child of the previous processed node
                 TreeNode* next = root->right;
                 while (next->left && next->left != root) {
                     next = next->left;
-                }
-              
-                // Establish a temporary link so we can return to the current node after traversing its right subtree
-                if (!next->left) {
-                    result.push_back(root->val); // Process the current node
-                    next->left = root; // Establish the temporary link
-                    root = root->right; // Move to the right subtree
+                
+                } if (!next->left) {
+                    result.push_back(root->val); 
+                    next->left = root; 
+                    root = root->right;
                 } else {
-                    next->left = nullptr; // Remove the temporary link
-                    root = root->left; // Move to the left subtree since the right subtree has been processed
+                    next->left = nullptr;
+                    root = root->left; 
                 }
             }
         }
-        // Reverse the results because the nodes were visited in reverse postorder
+      
         reverse(result.begin(), result.end());
         return result;
     }
