dfe28de45dce6965d5e945cd2052caa58e6786ed
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-03-05 22:44:46 +0630

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest String Chain/longest-string-chain.cpp
+++ Difficulty: Medium/Longest String Chain/longest-string-chain.cpp
@@ -13,13 +13,13 @@ class Solution {
         return a.length() < b.length();
     });
 
-    unordered_map<string, int> dp; // Store the longest chain ending at each word
+    unordered_map<string, int> dp; 
     int maxChain = 1;
 
     for (const string &word : words) {
-        dp[word] = 1; // Initialize chain length for the current word
+        dp[word] = 1; 
         for (int i = 0; i < word.length(); i++) {
-            // Generate the predecessor by removing one character
+            
             string predecessor = word.substr(0, i) + word.substr(i + 1);
             if (dp.find(predecessor) != dp.end()) {
                 dp[word] = max(dp[word], dp[predecessor] + 1);
