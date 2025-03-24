9892303d93c0e02ea84a05628bf97c2ef7e4af71
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-03-24 19:15:17 +0630

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.cpp
@@ -4,27 +4,33 @@ using namespace std;
 
 class Solution {
 public:
-    int countDays(int days, vector<vector<int>>& arr) {
-        // Sort the meetings based on their start and end days
-        sort(arr.begin(), arr.end());
-        
+    int countDays(int days, vector<vector<int>>& meetings) {
+        // Sort meetings by their ending day
+        sort(meetings.begin(), meetings.end(), [](vector<int>& a, vector<int>& b) {
+            return a[1] < b[1];
+        });
+
         int res = 0; // Count of days without meetings
-        int e = 0;   // Tracks the last meeting end day
-        
-        for (int i = 0; i < arr.size(); i++) {
-            // Count days between the previous end day and the start of the current meeting
-            if (arr[i][0] > e + 1) {
-                res += arr[i][0] - e - 1;
+        int lastMeetingEnd = 0; // Tracks the last meeting end day
+
+        for (auto& meeting : meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+
+            // Count days between the last meeting's end and the start of the current meeting
+            if (start > lastMeetingEnd + 1) {
+                res += start - lastMeetingEnd - 1;
             }
+
             // Update the last meeting end day
-            e = max(e, arr[i][1]);
+            lastMeetingEnd = max(lastMeetingEnd, end);
         }
-        
+
         // Count remaining days after the last meeting ends
-        if (days > e) {
-            res += days - e;
+        if (days > lastMeetingEnd) {
+            res += days - lastMeetingEnd;
         }
-        
+
         return res;
     }
 };
