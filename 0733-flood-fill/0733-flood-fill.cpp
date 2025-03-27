0621f58a3426ca1213294c3a8f6fb67f393d7fe9
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-03-27 08:01:08 +0630

    Time: 2 ms (18.1%), Space: 18.5 MB (23.56%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    typedef pair<int,int>P;
+     vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
+    vector<vector<int>> floodFill(vector<vector<int>>& arr, int sr, int sc, int color) {
+        int m = arr.size();
+        int n = arr[0].size();
+         int originalColor = arr[sr][sc];
+        if (originalColor == color) return arr;
+        queue<P>que;
+        que.push({sr,sc});
+
+        //arr[sr][sc]=color;
+        while(!que.empty()){
+            int len = que.size();
+            while(len--){
+                P curr = que.front();
+                que.pop();
+                int i = curr.first;
+                int j = curr.second;
+                 arr[i][j] = color;
+                for(vector<int>&dir:directions){
+                    int new_i=i+dir[0];
+                    int new_j=j+dir[1];
+                    if(new_i >=0 and new_i <m and new_j >= 0 and new_j <n and arr[new_i][new_j]== originalColor){
+                        
+                        que.push({new_i,new_j});
+                    }
+                }
+            }
+        }
+         return arr;
+      
+    }
+};
