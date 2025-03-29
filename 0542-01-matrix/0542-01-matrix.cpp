7dc4819d9b1f2ae6e9ab986f39426acb45c17d38
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-03-29 08:52:51 +0630

    Time: 22 ms (49.38%), Space: 34.8 MB (77.09%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,53 +1,52 @@
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
 class Solution {
 public:
-    typedef pair<int,int>P;
-    vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
-    int m;
-    int n;
-    int BFS(vector<vector<int>>& mat,int sr, int sc){
-         queue<P>que;
-         vector<vector<bool>> visited(m, vector<bool>(n, false));
-          visited[sr][sc] = true;
-        que.push({sr,sc});
-        int count =0;
-         while(!que.empty()){
-            int len = que.size();
-            while(len--){
-                P curr = que.front();
-                que.pop();
-                int i = curr.first;
-                int j = curr.second;
-                 //arr[i][j] = color;
-                 if(mat[i][j]==0) return count;
-                for(vector<int>&dir:directions){
-                    int new_i=i+dir[0];
-                    int new_j=j+dir[1];
-                    if(new_i >=0 and new_i <m and new_j >= 0 and new_j <n and !visited[new_i][new_j]){
-                          visited[new_i][new_j] = true;
-                        que.push({new_i,new_j});
-           
+    typedef pair<int, int> P;
+    vector<vector<int>> directions{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int m, n;
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        m = mat.size();
+        n = mat[0].size();
+        vector<vector<int>> res(m, vector<int>(n, INT_MAX)); // Initialize with INT_MAX
+        queue<P> que;
+
+        // Step 1: Enqueue all `0` cells and set their distance as 0
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    res[i][j] = 0;
+                    que.push({i, j});
+                }
+            }
+        }
+
+        // Step 2: Perform BFS
+        while (!que.empty()) {
+            P curr = que.front();
+            que.pop();
+            int i = curr.first;
+            int j = curr.second;
+
+            for (auto& dir : directions) {
+                int new_i = i + dir[0];
+                int new_j = j + dir[1];
+
+                // Check boundary conditions and relax distances
+                if (new_i >= 0 && new_i < m && new_j >= 0 && new_j < n) {
+                    if (res[new_i][new_j] > res[i][j] + 1) {
+                        res[new_i][new_j] = res[i][j] + 1;
+                        que.push({new_i, new_j});
                     }
                 }
-               
-            }
-            count++;
-        }
-        return -1;
-    }
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-         m = mat.size();
-         n = mat[0].size();
-        vector<vector<int>>res(m, vector<int>(n));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(mat[i][j] !=0){
-                    res[i][j] = BFS(mat,i,j);
-                }
-                else {
-                     res[i][j]=mat[i][j];
-                }
             }
         }
+
         return res;
     }
 };
