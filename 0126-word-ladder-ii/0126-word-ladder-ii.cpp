e78885ea3d572a45c263247eb1a4dc4ff2b9d4a5
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-04-02 10:38:43 +0630

    Time: 10 ms (85.18%), Space: 13.6 MB (50.24%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
+     void dfs(const string& word, const string& beginWord, 
+             unordered_map<string, vector<string>>& parents, 
+             vector<string>& path, vector<vector<string>>& result) {
+        path.push_back(word);
+        if (word == beginWord) {
+            result.push_back(vector<string>(path.rbegin(), path.rend()));
+        } else {
+            for (const string& parent : parents[word]) {
+                dfs(parent, beginWord, parents, path, result);
+            }
+        }
+        path.pop_back();
+    }
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string> dict(wordList.begin(), wordList.end());
-        if (dict.find(endWord) == dict.end()) return {}; // Ensure endWord is in wordList
+        if (dict.find(endWord) == dict.end()) return {}; 
 
-        unordered_map<string, vector<string>> parents; // To store predecessors
+        unordered_map<string, vector<string>> parents; 
         unordered_set<string> visited, currentLevelVisited;
         queue<string> que;
         que.push(beginWord);
@@ -25,19 +38,19 @@ public:
 
                         if (dict.find(temp) != dict.end() && visited.find(temp) == visited.end()) {
                             if (currentLevelVisited.find(temp) == currentLevelVisited.end()) {
-                                que.push(temp); // Add temp to queue once per level
+                                que.push(temp); 
                                 currentLevelVisited.insert(temp);
                             }
-                            parents[temp].push_back(word); // Track parent relationships
+                            parents[temp].push_back(word);
                             if (temp == endWord) found = true;
                         }
                     }
                 }
             }
-            visited.insert(currentLevelVisited.begin(), currentLevelVisited.end()); // Update visited after level
+            visited.insert(currentLevelVisited.begin(), currentLevelVisited.end()); 
         }
 
-        // Backtracking to construct unique paths
+       
         vector<vector<string>> result;
         vector<string> path;
         if (found) {
@@ -46,18 +59,6 @@ public:
         return result;
     }
 
-private:
-    void dfs(const string& word, const string& beginWord, 
-             unordered_map<string, vector<string>>& parents, 
-             vector<string>& path, vector<vector<string>>& result) {
-        path.push_back(word);
-        if (word == beginWord) {
-            result.push_back(vector<string>(path.rbegin(), path.rend())); // Reverse path for output
-        } else {
-            for (const string& parent : parents[word]) {
-                dfs(parent, beginWord, parents, path, result);
-            }
-        }
-        path.pop_back();
-    }
+
+   
 };
