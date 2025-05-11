2566b0a4a324a57a196ccc820db971fea7444560
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-05-11 11:51:49 +0630

    Time: 0 ms (100%), Space: 11.7 MB (98.85%) - LeetHub

---
+++ LeetCode/Easy/1550-three-consecutive-odds/1550-three-consecutive-odds-05-11-2025-10-52-38.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        bool flag = false;
+        for(int i=2;i<=arr.size()-1;i++){
+            if( arr[i-2]%2 != 0 and arr[i-1]%2 !=0 and arr[i]%2 != 0){
+                flag = true;
+                break;
+            }
+        }
+        if(flag ==false) return false;
+        return true;
+    }
+};
