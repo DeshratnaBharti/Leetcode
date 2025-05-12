e6c0e65a17245c1f5a3de1cf889decfeec490395
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-05-12 08:03:50 +0630

    Time: 3 ms (15.27%), Space: 24 MB (5.74%) - LeetHub

---
+++ LeetCode/Medium/0120-triangle/0120-triangle-05-12-2025-07-04-39.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int m;
+    vector<vector<int>>grid,dp;
+    int f(int i,int j){
+        if(i==m-1) return grid[i][j];
+        if(dp[i][j]!= -1) return dp[i][j];
+        return dp[i][j]=  grid[i][j]+min(f(i+1,j+1),f(i+1,j));
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        dp.clear();
+        dp.resize(205,vector<int>(205,-1));
+        grid = triangle;
+        m = grid.size();
+        return f(0,0);
+    }
+};
