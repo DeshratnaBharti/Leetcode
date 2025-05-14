6dafc02514ef0f4d5d2bfa51923dae06619aacdf
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-05-14 07:57:00 +0630

    Time: 27 ms (93.68%), Space: 13.8 MB (85.78%) - LeetHub

---
+++ LeetCode/Hard/1463-cherry-pickup-ii/1463-cherry-pickup-ii-05-14-2025-06-57-49.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public://top down Approach
+    int m, n;
+    int dp[71][71][71]; 
+    
+    int f(vector<vector<int>>& grid, int row, int c1, int c2) {
+        if (row >= m) return 0;
+        if (dp[row][c1][c2] != -1) return dp[row][c1][c2]; 
+        
+        int cherry = grid[row][c1];
+        if (c1 != c2) cherry += grid[row][c2];
+
+        int ans = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int newc1 = c1 + i;
+                int newc2 = c2 + j;
+                if (newc1 >= 0 && newc1 < n && newc2 >= 0 && newc2 < n) {
+                    ans = max(ans, f(grid, row + 1, newc1, newc2));
+                }
+            }
+        }
+        return dp[row][c1][c2] = ans + cherry;
+    }
+
+    int cherryPickup(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        memset(dp, -1, sizeof(dp)); 
+        return f(grid, 0, 0, n - 1);
+    }
+};
