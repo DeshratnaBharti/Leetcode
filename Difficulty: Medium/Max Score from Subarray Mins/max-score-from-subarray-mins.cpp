fc99cc4ed1f6c95a538029b419116522f11d5258
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-07-05 10:34:40 +0630

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Score from Subarray Mins/max-score-from-subarray-mins.cpp
+++ Difficulty: Medium/Max Score from Subarray Mins/max-score-from-subarray-mins.cpp
@@ -6,23 +6,11 @@ public:
 
         for (int i = 0; i < n - 1; ++i) {
             int a = arr[i], b = arr[i + 1];
-            int min1 = min(a, b), min2 = max(a, b);
-            max_score = max(max_score, min1 + min2);
+           
+            max_score = max(max_score, a + b);
 
             // Expand to left and right to try to include bigger subarray
-            int l = i - 1, r = i + 2;
-            while (l >= 0 && r < n) {
-                int x = arr[l], y = arr[r];
-
-                // Only expand if both elements are ≥ min2
-                if (x >= min2 && y >= min2) {
-                    max_score = max(max_score, min1 + min2);
-                    l--;
-                    r++;
-                } else {
-                    break;
-                }
-            }
+           
         }
 
         return max_score;
