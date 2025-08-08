7c12d6df5e1b78a8496a7e72f1875b00ac425009
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-08-08 07:26:34 +0630

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/0808-soup-servings/0808-soup-servings-08-08-2025-06-27-34.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<double>>t;
+    vector<pair<int,int>>serves = {{100,0},{75,25},{50,50},{25,75}};
+    double solve(double A, double B){
+        if(A<=0 and B<=0) return 0.5;
+        if(A<=0) return 1.0;
+        if(B<=0) return 0.0;
+        if(t[A][B] != -1.0) return t[A][B];
+        double prob =0.0;
+        for(auto &p:serves){
+            int A_taken = p.first;
+            int B_taken = p.second;
+            prob += solve(A-A_taken,B - B_taken);
+        }
+        return t[A][B]=0.25*prob;
+    }
+    double soupServings(int n) {
+        if(n>=4000) return 1.0;
+        t.resize(n+1,vector<double>(n+1,-1.0));
+        return solve(n,n);
+        
+    }
+};
