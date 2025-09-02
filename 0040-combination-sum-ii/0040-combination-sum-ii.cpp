4cd945278322801bf581c409896fea81a436470e
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-09-02 08:07:01 +0530

    Time: 2 ms (46.69%), Space: 14 MB (60.4%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -6,16 +6,16 @@ public:
             ans.push_back(temp);
             return;
         }
-  for(int i=idx;i<candidates.size();i++){
-    if(i != idx and candidates[i] == candidates[i-1]) continue;
-    temp.push_back(candidates[i]);
-    solve(i+1,ans,temp,candidates,target-candidates[i]);
-     temp.pop_back();
-    } 
+    for(int i=idx;i<candidates.size();i++){
+            if(i != idx and candidates[i] == candidates[i-1]) continue;
+            temp.push_back(candidates[i]);
+            solve(i+1,ans,temp,candidates,target-candidates[i]);
+            temp.pop_back();
+            } 
      } 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
-         vector<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int>temp;
         
         solve(0,ans,temp,candidates,target);
