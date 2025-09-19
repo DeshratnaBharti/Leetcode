aa1a1cf25be363ec2e69036050f96db02d5dd9c3
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-09-19 20:55:20 +0530

    Time: 0 ms (100%), Space: 16.3 MB (99.89%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,34 +10,25 @@
  */
 class Solution {
 public:
-    int lengthOfLinkedList(ListNode* head){
-        int length =0;
-        ListNode* temp = head;
-        while(temp){
-            temp=temp->next;
-            length++;
-            
-        }
-        return length;
-    }
-    ListNode* reverseK(ListNode* head, int k,int length){
-        if(length < k) return head;
-        int count =0;
-        ListNode* prev = NULL,*nex = NULL,*curr = head;
-        while(count<k and curr){
-            nex= curr->next;
-            curr->next = prev;
-            prev =curr;
-            curr = nex;
-            count++;
-        }
-        if(nex){
-            head->next = reverseK(nex,k,length-k);
-        }
-        return prev;
-    }
+    
     ListNode* reverseKGroup(ListNode* head, int k) {
-        int length = lengthOfLinkedList(head);
-        return  reverseK(head,k,length);
+        ListNode* temp = head;
+        int cnt =0;
+        while(cnt<k){
+            if(temp==NULL) return head;
+            temp = temp->next;
+            cnt++;
+        }
+        ListNode* prevNode = reverseKGroup(temp,k);
+        temp=head;cnt=0;
+        while(cnt<k){
+            ListNode* next = temp->next;
+            temp->next = prevNode;
+            prevNode = temp;
+            temp=next;
+            cnt++;
+        }
+        return prevNode;
+        
     }
 };
