debc37138fa22e6d8ef8c2682032686bcb957172
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-09-30 17:20:09 +0530

    Time: 81 ms (5.17%), Space: 23.2 MB (94.36%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,40 +1,28 @@
 class MinStack {
 public:
-    /** initialize your data structure here. */
-    stack<int> stk;
-    int m = INT_MAX;
+vector<int> v;
     MinStack() {
+        vector<int> v;
     }
     
     void push(int x) {
-        if(x <= m){ //note it's <=, not <!
-            //2nd smallest value is under 1st smallest value
-            stk.push(m);
-            m = x;
-        }
-        stk.push(x);
+        v.push_back(x);
     }
     
     void pop() {
-        int tmp = stk.top();
-        stk.pop();
-        //pop 2nd smallest value along 1st smallest value
-        if(tmp == m){
-            //when pop 1st smallest value
-            //update m to 2nd smallest value
-            m = stk.top();
-            stk.pop();
-        }
+        v.erase(v.begin()+v.size()-1);
     }
     
     int top() {
-        return stk.top();
+        //return v[v.size()-1];
+        return v.back();
     }
     
     int getMin() {
-        return m;
+        return *min_element(v.begin(), v.end());
     }
 };
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
