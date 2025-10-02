e5e41e0bd0d79453261fa055a693969b56cfa899
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-10-02 17:45:03 +0530

    Time: 1 ms (40.39%), Space: 17 MB (46.11%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,38 +2,47 @@ class Solution {
 public:
     typedef pair<int,int>P;
     vector<vector<int>>directions{{-1,0},{1,0},{0,-1},{0,1}};
+    bool isSafe(int i, int j, int m, int n, vector<vector<int>>& grid) {
+        return i >= 0 && i < m && j >= 0 && j < n && grid[i][j] == 1;
+    }
     int orangesRotting(vector<vector<int>>& grid) {
-        int m =grid.size();
-        int n = grid.size();
+        int m = grid.size();
+        int n = grid[0].size();
         queue<P>que;
-        int freshCount =0;
+        int f=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1) freshCount++;
-                else if(grid[i][j]==2) que.push({i,j});
+                if(grid[i][j]==2){
+                    que.push({i,j});
+                }else if(grid[i][j]==1){
+                    f++;
+                }
             }
         }
-        if(freshCount == 0) return 0;
-        int minute =0;
+        if(f==0)return 0;
+        int min=0;
         while(!que.empty()){
-            int len = que.size();
-            while(len--){
+            int N= que.size();
+            while(N--){
                 P curr = que.front();
                 que.pop();
                 int i = curr.first;
                 int j = curr.second;
-                for(vector<int>& dir: directions){
+                for(auto &dir:directions){
                     int new_i = i+dir[0];
                     int new_j = j+dir[1];
-                    if(new_i >= 0 and new_i <m and new_j >=0 and new_j<n and grid[new_i][new_j]==1){
+                    if(isSafe(new_i, new_j, m, n, grid)){
                         grid[new_i][new_j]=2;
                         que.push({new_i,new_j});
-                        freshCount--;
+                        f--;
+
                     }
                 }
             }
-            minute++;
+            min++;
+
         }
-        return freshCount==0 ? (minute-1): -1;
+        return f==0 ? min-1:-1;
+        
     }
 };
