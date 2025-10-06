231954b25e1e498ead689dce76b4c19c3d4d7c00
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-10-06 23:05:26 +0530

    Time: 0 ms (100%), Space: 10.4 MB (99.28%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -2,45 +2,23 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-          stack<TreeNode*> st;
-    // Start from the root node
-    TreeNode* node = root;
-    // Initialize a vector to store
-    // inorder traversal result
-    vector<int> inorder;
-
-    // Start an infinite
-    // loop for traversal
-    while(true){
-        // If the current node is not NULL
-        if(node != NULL){
-            // Push the current
-            // node to the stack
-            st.push(node);
-            // Move to the left child
-            // of the current node
-            node = node->left;
-        }
-        else{
-            // If the stack is empty,
-            // break the loop
-            if(st.empty()){
-                break;
+        vector<int>ans;
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(curr->left == NULL){
+                ans.push_back(curr->val);
+                curr= curr->right;
+            }else{
+                TreeNode* leftChild = curr->left;
+                while(leftChild->right != NULL){
+                    leftChild = leftChild->right;
+                }
+                leftChild->right = curr;
+                TreeNode* temp = curr;
+                curr= curr->left;
+                temp->left = NULL;
             }
-            // Retrieve a node from the stack
-            node = st.top();
-            // Remove the node from the stack
-            st.pop();
-            // Add the node's value to
-            // the inorder traversal list
-            inorder.push_back(node->val);
-            // Move to the right child
-            // of the current node
-            node = node->right;
         }
-    }
-    // Return the inorder
-    // traversal result
-    return inorder;
+        return ans;
     }
 };
