8ee3e3fa53d4071fc7a279482e4feacd41c95a08
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-10-12 20:42:48 +0530

    Time: 284 ms (6.81%), Space: 22.1 MB (99.94%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,37 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int maxDiameter;
+int maxDia =0;
+int levels(TreeNode* root){
+    if(root==NULL) return 0;
+    return 1+max(levels(root->left),levels(root->right));
+}
     int diameterOfBinaryTree(TreeNode* root) {
-         maxDiameter = 0;
-        depthFirstSearch(root);
-        return maxDiameter;
-    }
+        if(root == NULL) return 0;
+        int dia = levels(root->left)+levels(root->right);
+        maxDia=max(maxDia,dia);
+        diameterOfBinaryTree(root->left);
+        diameterOfBinaryTree(root->right);
+        return maxDia;
 
-    // Helper function for DFS traversal of the tree.
-    // Calculates the depth of the tree and updates the maximum diameter.
-    int depthFirstSearch(TreeNode* node) {
-        if (node == nullptr) return 0;  // Base case: return zero for null nodes.
-
-        // Recursive DFS on the left child.
-        int leftDepth = depthFirstSearch(node->left);
-        // Recursive DFS on the right child.
-        int rightDepth = depthFirstSearch(node->right);
-
-        // Update the maximum diameter if the current node's diameter is greater.
-        maxDiameter = max(maxDiameter, leftDepth + rightDepth);
-
-        // Return the maximum depth from this node down to the leaf.
-        return 1 + max(leftDepth, rightDepth);
     }
 };
