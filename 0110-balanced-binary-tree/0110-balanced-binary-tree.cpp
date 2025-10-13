736fcb4f74558bf8a542cdd659cd8eb3f738b43b
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-10-13 20:24:46 +0530

    Time: 0 ms (100%), Space: 23.1 MB (56.75%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,27 +1,22 @@
-
 class Solution {
 public:
-    bool isBalanced(TreeNode* root) {
-        std::function<int(TreeNode*)> findHeight = [&](TreeNode* node) -> int {
-            if (!node) {
-                // Tree is empty or this is a leaf node, hence the height is 0.
-                return 0;
-            }
-            int leftHeight = findHeight(node->left);
-            int rightHeight = findHeight(node->right);
+    int height(TreeNode* root) {
+        if (root == nullptr) return 0;
 
-            if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) {
-                // If the left or right subtree is unbalanced, or if the current subtree
-                // is unbalanced (difference in height > 1), return -1 as an indicator.
-                return -1;
-            }
-            // Otherwise, calculate the height of this subtree,
-            // which is 1 + the maximum height of its left or right subtree.
-            return 1 + std::max(leftHeight, rightHeight);
-        };
-      
-        // If the height function returns -1, the tree is unbalanced.
-        // Otherwise, it is balanced.
-        return findHeight(root) >= 0;
+        int left = height(root->left);
+        int right = height(root->right);
+
+       
+        if (left == -1 || right == -1) return -1;
+
+        
+        if (abs(left - right) > 1) return -1;
+
+        // warna height return kar de
+        return max(left, right) + 1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return height(root) != -1;
     }
 };
