ba518ef892b0af1fdc0b5c3d297274706dcd6618
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-10-16 08:16:27 +0530

    Time: 0 ms (100%), Space: 15 MB (60.86%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -2,35 +2,22 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-         vector<int> rightView; // Stores the right side view elements
-        if (!root) {
-            return rightView; // If the root is null, return an empty vector
-        }
-
-        queue<TreeNode*> nodesQueue; // Queue to perform level order traversal
-        nodesQueue.push(root);
-
-        while (!nodesQueue.empty()) {
-            // Add the rightmost element of current level to the right view
-            rightView.emplace_back(nodesQueue.back()->val);
-
-            // Traverse the current level
-            for (int levelSize = nodesQueue.size(); levelSize > 0; --levelSize) {
-                TreeNode* currentNode = nodesQueue.front();
-                nodesQueue.pop();
-
-                // If left child exists, add it to the queue for next level
-                if (currentNode->left) {
-                    nodesQueue.push(currentNode->left);
-                }
-
-                // If right child exists, add it to the queue for next level
-                if (currentNode->right) {
-                    nodesQueue.push(currentNode->right);
-                }
+        if(root==NULL) return {};
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<int>ans;
+        while(!q.empty()){
+            int n = q.size();
+             TreeNode* node =NULL;
+            while(n--){
+                node = q.front();
+                q.pop();
+                if(node->left != NULL)q.push(node->left);
+                if(node->right != NULL)q.push(node->right);
             }
+            ans.push_back(node->val);
         }
+        return ans;
 
-        return rightView;
     }
 };
