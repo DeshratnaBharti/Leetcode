a4b404d1e850cea8fa3422211a47a3a16f5139dc
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-11-02 21:38:19 +0530

    Time: 0 ms (100%), Space: 60.7 MB (80.92%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -7,24 +7,16 @@ public:
 
     void inorder(TreeNode* root) {
         if (!root) return;
-
-       
         inorder(root->left);
-
-      
         if (prev && prev->val > root->val) {
             if (!first) first = prev;
             second = root;
         }
         prev = root;
-
-        
         inorder(root->right);
     }
     void recoverTree(TreeNode* root) {
           inorder(root);
-
-        
         if (first && second) {
             swap(first->val, second->val);
         }
