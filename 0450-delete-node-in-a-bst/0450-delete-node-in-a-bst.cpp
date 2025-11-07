2ef6ac8cf5bc7fe85c6b0da5f7755cfbd118c9d9
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-11-07 19:11:24 +0530

    Time: 0 ms (100%), Space: 34.3 MB (72.7%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -8,26 +8,20 @@ public:
         return pred;
     }
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root==NULL) return NULL;
-        if(root->val ==key){
-            if(root->left ==NULL and root->right==NULL) return NULL;
-            if(root->left==NULL or root->right==NULL){
-                if(root->left != NULL) return root->left;
-                else return root->right;
-            }
-            //case 3 : 2 child Nodes
-            if(root->left !=NULL and root->right != NULL){
-                TreeNode* pred = iop(root);
-                root->val = pred->val;
-                root->left=deleteNode(root->left,pred->val);
+      if (!root) return nullptr;
 
-            } 
-        }
-        else if(root->val >key){
-            root->left = deleteNode(root->left,key);
-
-        }else{
-            root->right = deleteNode(root->right,key);
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            
+            if (!root->left) return root->right; 
+            if (!root->right) return root->left; 
+            
+            TreeNode* pred = iop(root);
+            root->val = pred->val;
+            root->left = deleteNode(root->left, pred->val);
         }
         return root;
     }
