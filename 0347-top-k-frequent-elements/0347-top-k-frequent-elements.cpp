6826e6370dbfc3bb9a41728093ecb1b2e187c817
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-11-10 09:00:56 +0530

    Time: 3 ms (49.69%), Space: 18 MB (61.05%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-            // Step 1: Count the frequency of each element
-    unordered_map<int, int> freqMap;
-    for (int num : nums) {
-        freqMap[num]++;
-    }
-
-    // Step 2: Store the elements and their frequencies in a list of pairs
-    vector<pair<int, int>> freqList;
-    for (const auto& entry : freqMap) {
-        freqList.push_back(entry);
-    }
-
-    // Step 3: Sort the list based on frequency in descending order
-    sort(freqList.begin(), freqList.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-        return a.second > b.second;
-    });
-
-    // Step 4: Select the top K elements
-    vector<int> result;
-    for (int i = 0; i < k; ++i) {
-        result.push_back(freqList[i].first);
-    }
+       unordered_map<int, int> freq;
+       for (int num : nums) freq[num]++;
+         auto cmp = [](pair<int, int>& a, pair<int, int>& b) {
+              return a.second > b.second; 
+         };
+         priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
+         for (auto& [num, count] : freq) {
+            pq.push({num, count});
+            if (pq.size() > k) pq.pop(); 
+        }
+         vector<int> result;
+            while (!pq.empty()) {
+                result.push_back(pq.top().first);
+                pq.pop();
+            }
 
     return result;
     }
