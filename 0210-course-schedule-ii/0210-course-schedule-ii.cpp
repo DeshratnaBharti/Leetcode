b292e303ef58063fa69b8d3016ff40b30e130121
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-12-11 08:41:37 +0530

    Time: 9 ms (18.15%), Space: 18.8 MB (42.59%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
-    vector<int> topSort(vector<vector<int>>& adj, vector<int>& indegree, int n) {
-        queue<int> que;
-        vector<int> result;
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                result.push_back(i);
-                que.push(i);
+    vector<int>topoSort(unordered_map<int,vector<int>>&adj,vector<int>&indegree,int n){
+        queue<int>que;
+        int count =0;
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
                 count++;
+                ans.push_back(i);
+                que.push(i);
             }
         }
-
-        while (!que.empty()) {
-            int a = que.front();
+        while(!que.empty()){
+            int curr = que.front();
             que.pop();
-
-            for (int& b : adj[a]) {
-                indegree[b]--;
-                if (indegree[b] == 0) {
-                    result.push_back(b);
-                    que.push(b);
+            for(auto &v:adj[curr]){
+                indegree[v]--;
+                if(indegree[v]==0){
                     count++;
+                    que.push(v);
+                    ans.push_back(v);
                 }
             }
         }
-
-        if (count == n) return result;
+        if(count==n) return ans;
         return {};
     }
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
-
-        for (auto& vec : prerequisites) {
+        unordered_map<int,vector<int>>adj;
+        vector<int>indegree(numCourses,0);
+        for(auto &vec:prerequisites){
             int a = vec[0];
             int b = vec[1];
             adj[b].push_back(a);
             indegree[a]++;
         }
-
-        return topSort(adj, indegree, numCourses);
+        return topoSort(adj,indegree,numCourses);
     }
 };
