c3c5498b47c403a521a36f70a44183e64311638b
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-12-27 14:59:57 +0530

    Time: 282 ms (5.03%), Space: 82.9 MB (5.77%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> directions{{1,1},{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{1,-1},{-1,1}};
+    typedef pair<int,pair<int,int>>P;
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
@@ -8,30 +9,27 @@ public:
         auto isSafe = [&](int x,int y){
             return x>=0 and x<n and y>=0 and y<n;
         };
-        queue<pair<int,int>>que;
-        que.push({0,0});
-        grid[0][0] =1;
-        int level =0;
-        while(!que.empty()){
-            int N = que.size();
-            while(N--){
-                auto curr = que.front();
-                que.pop();
-                int x = curr.first;
-                int y = curr.second;
-                if(x==m-1 and y==n-1) return level+1;
-
-                for(auto dir: directions){
-                    int x_new = x + dir[0];
-                    int y_new = y + dir[1];
-                    if(isSafe(x_new,y_new) and grid[x_new][y_new] == 0){
-                        que.push({x_new,y_new});
-                        grid[x_new][y_new] = 1;
-                    }
+        vector<vector<int>>result(m,vector<int>(n,INT_MAX));
+        priority_queue<P,vector<P>,greater<P>>pq;
+        pq.push({0,{0,0}});
+        result[0][0] = 0;
+        while(!pq.empty()){
+            int d = pq.top().first;
+            pair<int,int>node = pq.top().second;
+            int x = node.first;
+            int y = node.second;
+            pq.pop();
+            for(auto dir:directions){
+                int x_new = x + dir[0];
+                int y_new = y+ dir[1];
+                int dist = 1;
+                if(isSafe(x_new,y_new) and grid[x_new][y_new]==0 and d + dist < result[x_new][y_new]){
+                    pq.push({d + dist,{x_new,y_new}});
+                    result[x_new][y_new] = d + dist;
                 }
             }
-            level++;
         }
-        return -1;
+        if(result[m-1][n-1]==INT_MAX) return -1;
+        return result[m-1][n-1]+1;
     }
 };
