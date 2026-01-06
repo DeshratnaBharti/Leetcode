a6f49b6232a5f61846579e6472971110f3c0915a
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-01-06 11:59:49 +0530

    Time: 72 ms (24.07%), Space: 73.4 MB (22.98%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int DFS(unordered_map<int,vector<int>>&adj,int curr,int parent,vector<bool>&hasApple){
+        int time =0;
+        for(int &child : adj[curr]){
+            if(child == parent) continue;
+            int t_f_c = DFS(adj,child,curr,hasApple);
+            if(t_f_c > 0 or hasApple[child]){
+                time += t_f_c +2;
+            }
+        }
+        return time;
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        unordered_map<int,vector<int>>adj;
+        for(auto &vec : edges){
+            int u = vec[0];
+            int v = vec[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        return DFS(adj,0,-1,hasApple);
+    }
+};
