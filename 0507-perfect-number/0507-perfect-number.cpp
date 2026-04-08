4bf5b24f776bb79645d8f7aaf2bfac37ce674e5e
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-04-08 10:06:00 +0530

    Time: 1063 ms (37.37%), Space: 7.7 MB (54.28%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int s =0;
+        for(int i=1;i<=num/2;i++){
+           if(num %i ==0){
+            s = s+i;
+           }
+        }
+        if(s ==num) return true;
+        return false;
+    }
+};
