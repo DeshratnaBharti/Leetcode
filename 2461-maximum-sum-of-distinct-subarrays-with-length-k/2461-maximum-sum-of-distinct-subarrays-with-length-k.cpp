49f53dc37168ae914a73a2de244c8a7adc8e16a8
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-04-18 19:26:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -4,19 +4,19 @@ public:
         long long n = nums.size();
         long long s = 0,i=0;
         long long maxi = 0;
-        unordered_map<int,int>mp;
+        unordered_set<int>st;
         
         for(int j=0;j<n;j++){
              s = s + nums[j];
-             mp[nums[j]]++;
+             st.insert(nums[j]);
              if(j-i+1>k){
-                 mp[nums[i]]--;
+                 
                 s = s-nums[i];
                 //mp[nums[i]]--;
-                if(mp[nums[i]]==0) mp.erase(nums[i]);
+                 st.erase(nums[i]);
                 i++;
              }
-             if(j-i+1==k and mp.size()==k)
+             if(j-i+1==k and st.size()==k)
             
               maxi = max(maxi,s);
         }
