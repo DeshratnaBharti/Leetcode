f801a75ab88d832639ec95c2c6a94f21b57cf4f3
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-04-20 15:50:35 +0530

    Time: 0 ms (100%), Space: 23.5 MB (82.82%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
         int c1 =0,c2 =0;
-        int maj1 = NULL,maj2 = NULL;
+        int maj1 = INT_MIN,maj2 = INT_MIN;
         for(int i=0;i<n;i++){
             if(nums[i] == maj1) c1++;
             else if(nums[i]==maj2) c2++;
-            else if(maj1 ==0){
+            else if(c1==0){
                 maj1 = nums[i];
-                c1++;
+                c1=1;
 
-            }else if(maj2 ==0){
+            }else if(c2==0){
                 maj2 = nums[i];
-                c2++;
+                c2=1;
             }else {
                 c1--;
                 c2--;
@@ -22,11 +22,13 @@ public:
         c1=0,c2=0;
         for(auto a:nums){
             if(a ==maj1)c1++;
-            if(a == maj2)c2++;
+            else if(a == maj2)c2++;
         }
+
         vector<int>ans;
-        if(c1>n/3) ans.push_back(maj1);
-        if(c2>n/3) ans.push_back(maj2);
+        if(c1> n/3) ans.push_back(maj1);
+        if(c2> n/3) ans.push_back(maj2);
+       // if(c1==c2) return {maj1};
         return ans;
     }
 };
