8e0752ff147a706c9a333bbc118e3c03ab07437e
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-04-21 13:59:17 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.23%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,19 +3,20 @@ public:
     bool isValid(string s) {
        stack<char>st;
         for(int i=0;i<s.size();i++){
+            char ch = s[i];
             if(s[i]=='(' or s[i]=='{' or s[i] =='[') st.push(s[i]);
-            else if(s[i]==')'){
-                if(st.empty() or st.top() !='(') return false;
+            else {
+                if(!st.empty() and  ch ==')' and  st.top() =='(') 
                 st.pop();
-            }
-             else if(s[i]=='}'){
-                if(st.empty() or st.top() !='{') return false;
+           
+           else  if(!st.empty() and  ch =='}' and  st.top() =='{') 
                 st.pop();
-            } 
-            else if(s[i]==']'){
-                if(st.empty() or st.top() !='[') return false;
+            
+           else if(!st.empty() and ch ==']' and  st.top() =='[') 
                 st.pop();
-            }
+            else return false;    
+
+        }
         }
          return st.empty();
     }
