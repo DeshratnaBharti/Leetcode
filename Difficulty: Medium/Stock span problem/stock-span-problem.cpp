1ec87bb8628fb6f5e82c4d58967b50b0ac614a9f
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-04-25 08:07:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
+        // code here
+        stack<int>st;
+        int n  = arr.size();
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            int count =1;
+            while(!st.empty() and arr[st.top()]<=arr[i]){
+              st.pop();
+              count++;
+           }
+            if (st.empty()) {
+               ans[i] = (i + 1);
+           }
+           else {
+              ans[i] = (i - st.top());
+        
+            }
+            st.push(i);
+        }
+        return ans;
+        
+    }
+};
