3ceb157c86cd3838ac1e19cb0e04ab03e2bed534
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-04-27 08:38:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,23 +1,14 @@
-import java.util.Arrays;
-
 class Solution {
-    int n;
-    int[] t;
-
-    int solve(int[] nums, int i) {
-        if (i >= n) return 0;
-        if (t[i] != -1) return t[i];
-
-        int take = nums[i] + solve(nums, i + 2);
-         int notTake = solve(nums, i + 1);
-
-         return t[i]= Math.max(take, notTake);
-    }
-
     public int rob(int[] nums) {
-        n = nums.length;
-        t = new int[101];
-         Arrays.fill(t, -1);
-        return solve(nums, 0);
-    }
+        int n = nums.length;
+        int[] t = new int[n+1];
+        t[0] = 0;
+        t[1] = nums[0];
+        for(int i=2;i<=n;i++){
+            int take = nums[i-1]+t[i-2];
+            int not_take = t[i-1];
+            t[i] = Math.max(take,not_take);
+        }
+        return t[n];
+     }
 }
