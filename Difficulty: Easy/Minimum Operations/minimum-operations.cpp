d5abae1baf9cf1130a0edde28300c92fb77aa1a8
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-04-28 11:14:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Operations/minimum-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int minOperation(int n) {
+        int count = 0;
+        while (n > 1) {
+            if (n % 2 == 0)
+                n /= 2;
+            else
+                n -= 1;
+            count++;
+        }
+        return 1+count;
+
+    }
+};
