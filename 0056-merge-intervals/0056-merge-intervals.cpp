bbd708ade4632693fe6b530bd418fa02117dc651
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-04-28 08:09:52 +0530

    Time: 7 ms (41.12%), Space: 23.8 MB (81.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-        int m = arr.size();
-        int n = arr[0].size();
         vector<vector<int>>ans;
-         sort(arr.begin(),arr.end());
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
         ans.push_back({arr[0][0],arr[0][1]});
-        //sort(arr.begin(),arr.end());
-        for(int i=1;i<m;i++){
-            int a = ans.back()[0];
-            int b  = ans.back()[1];
-            int c = arr[i][0];
-            int d = arr[i][1];
-            if(b>=c){
-                ans.back()[1] = max(b,d);
-
-            }else{
-                 ans.push_back({c,d});
+        for(int i=1;i<n;i++){
+            int a = ans.back()[1];
+            int b = arr[i][0];
+            int c = arr[i][1];
+            if(a>=b){
+                ans.back()[1]= max(a,c);
             }
-
-
+            else{
+                ans.push_back({b,c});
+            }
         }
         return ans;
     }
