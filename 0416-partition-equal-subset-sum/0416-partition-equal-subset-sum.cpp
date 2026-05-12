b10e220c8726698a6433e8d83f823faa5861ba5f
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-05-12 20:29:57 +0530

    Time: 207 ms (9.86%), Space: 17.3 MB (62.94%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -13,13 +13,33 @@ public:
 
 
     }
-    bool canPartition(vector<int>& nums) {
-       int n = nums.size(); 
+    bool canPartition(vector<int>& arr) {
+       int n = arr.size(); 
          
-        int s= accumulate(nums.begin(),nums.end(),0);
+        int s= accumulate(arr.begin(),arr.end(),0);
         if(s%2 !=0) return false;
-        int tgt = s/2;
-        dp.resize(n,vector<int>(tgt+1,-1));
-        return solve(nums,n-1,0,tgt);
+        int target = s/2;
+         vector<vector<bool>> dp(n+1, vector<bool>(target+1, false));
+
+        
+        for(int i=0; i<=n; i++){
+            dp[i][0] = true;
+        }
+        
+        for(int j=1; j<=target; j++){
+            dp[0][j] = false;
+        }
+
+        // Fill DP table
+        for(int i=1; i<=n; i++){
+            for(int sum=1; sum<=target; sum++){
+                dp[i][sum] = dp[i-1][sum]; // skip
+                if(sum >= arr[i-1]) {
+                    dp[i][sum] = dp[i][sum] || dp[i-1][sum - arr[i-1]]; // take
+                }
+            }
+        }
+
+        return dp[n][target];
     }
 };
