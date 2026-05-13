4b87dc4294f64da9470b2f13341e311735fa137f
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-05-13 17:36:28 +0530

    Time: 41 ms (18.07%), Space: 23.8 MB (70.4%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
+    vector<vector<int>>directions{{1,1},{0,1},{0,-1},{-1,-1},{-1,0},{-1,1},{1,-1},{1,0}};
+    typedef pair<int,pair<int,int>>P;
     int m,n;
-    vector<vector<int>>directions{{1,1},{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{1,-1},{-1,1}};
     bool isSafe(int x,int y){
-        if(x>=0 and y>=0 and x<m and y<n ) return true;
-        return false;
+        if(x>=m or y>=n or x<0 or y<0) return false;
+        return true;
+
     }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        priority_queue<P,vector<P>,greater<P>>que;
          m = grid.size();
          n = grid[0].size();
-         if(m==0 or n==0 or grid[0][0] != 0) return -1;
-        queue<pair<int,int>>que;
-        grid[0][0] =1;
-        que.push({0,0});
-        int count =0;
-        while(!que.empty()){
-            int N= que.size();
-            while(N--){
-                auto curr = que.front();
-                que.pop();
-                int x = curr.first;
-                int y = curr.second;
-                if(x==m-1 and y==n-1) return count+1;
-                for(auto &dir:directions){
-                    int new_x = x + dir[0];
-                    int new_y = y+dir[1];
-                    if(isSafe(new_x,new_y) and grid[new_x][new_y] == 0){
-                        que.push({new_x,new_y});
-                        grid[new_x][new_y] = 1;
-                    }
-                }
-            }
-            count++;
-        }
-        return -1;
+        if(m==0 or n ==0 or grid[0][0] != 0) return -1;
+         vector<vector<int>>res(m,vector<int>(n,INT_MAX));
+         que.push({0,{0,0}});
+         res[0][0] = 0;
+         while(!que.empty()){
+            auto curr = que.top();
+            que.pop();
+            int pahleWalaDist = curr.first;
+            pair<int,int>v= curr.second;
+            int x = v.first;
+            int y = v.second;
+            for(auto &dir:directions){
+                int new_x = x+dir[0];
+                int new_y = y+dir[1];
+                int currDist = 1;
+                if(isSafe(new_x,new_y) and grid[new_x][new_y] != 1 and res[new_x][new_y] > pahleWalaDist + currDist){
+                    res[new_x][new_y] = pahleWalaDist + currDist;
+                    que.push({res[new_x][new_y],{new_x,new_y}});
+
+                }          
+            }    
+         }
+          if(res[m-1][n-1]==INT_MAX) return -1;
+        return res[m-1][n-1]+1;
+        
     }
 };
