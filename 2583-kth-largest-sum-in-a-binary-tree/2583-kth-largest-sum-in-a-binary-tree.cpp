14ce7684f5e3934e657ec7ea5f11edd9084e1632
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-05-14 10:36:42 +0530

    Time: 37 ms (47.27%), Space: 266.9 MB (51.02%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
@@ -1,28 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        if (root == NULL) return -1;
-
-        vector<long long> s;
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int n = q.size();
+        queue<TreeNode*>que;
+        que.push(root);
+        vector<long long>s;
+        while(!que.empty()){
+            int N = que.size();
             long long sum = 0;
+            while(N--){
+                auto node = que.front();
+                que.pop();
+                sum += node->val;
+                if(node->left != nullptr) que.push(node->left);
+                if(node->right != nullptr) que.push(node->right);
 
-            while (n--) {
-                TreeNode* p = q.front(); 
-                q.pop();
-                sum += p->val;
-                if (p->left) q.push(p->left);
-                if (p->right) q.push(p->right);
+
             }
-
             s.push_back(sum);
         }
+        if(k > s.size()) return -1;
         sort(s.begin(),s.end());
-        if (k > s.size()) return -1;
-        return s[s.size() - k];
+        return s[s.size()-k];
+        
     }
 };
