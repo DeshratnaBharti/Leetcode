ca3efcca210701dd77c32c1e9af2f6f44cf50dfb
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2026-09-01 07:55:35 +0530

    Time: 45 ms (70.25%), Space: 33.1 MB (13.04%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
- vector<vector<int>> result;
- void twosum(vector<int>& nums,int tgt,int j, int k){
-    while(j<k){
-        if(nums[j]+nums[k]> tgt)k--;
-        else  if(nums[j]+nums[k]<tgt) j++;
-        else{
-            while(j<k and nums[j]==nums[j+1]) j++;
-             while(j<k and nums[k]==nums[k-1]) k--;
-             result.push_back({-tgt,nums[j],nums[k]});
-             j++; k--;
+    vector<vector<int>>ans;
+    void twoSum(vector<int>&nums,int tgt,int i,int j){
+        while(i<j){
+            if(nums[i] + nums[j] > tgt) j--;
+            else if(nums[i] + nums[j] < tgt) i++;
+            else{
+                while(i<j and nums[i] == nums[i+1]) i++;
+                while(i<j and nums[j]== nums[j-1]) j--;
+                ans.push_back({-tgt,nums[i],nums[j]});
+                i++,j--;
+            }
         }
     }
- }
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
-        if(n<3) return {};
-        result.clear();
         sort(nums.begin(),nums.end());
-         //ek element ko fix kar do
-         for(int i=0;i<n;i++){
-            if(i>0 and nums[i]==nums[i-1]) continue;
-            int n1 = nums[i];
-            int target =-n1;
-            twosum(nums,target,i+1,n-1);
-         }
-        return result;
+       // vecotr<vector<int>>ans;
+        for(int i=0;i<n;i++){
+            if(i> 0 and nums[i] == nums[i-1]){
+                continue;
+            }
+            int n1= nums[i];
+            int tgt = -n1;
+            twoSum(nums,tgt,i+1,n-1);
+            
+        }
+        return ans;
+        
     }
 };
